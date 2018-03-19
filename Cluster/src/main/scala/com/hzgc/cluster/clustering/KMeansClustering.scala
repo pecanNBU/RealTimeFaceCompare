abd0d24cefe31258e5248e14db9a6133efdff5b8
personalc <personalc@163.com>
2018-03-19 09:05:08 +0800

    test

--- Cluster/src/main/scala/com/hzgc/cluster/clustering/KMeansClustering.scala
+++ Cluster/src/main/scala/com/hzgc/cluster/clustering/KMeansClustering.scala
@@ -3,7 +3,7 @@ package com.hzgc.cluster.clustering
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util
-import java.util.{Calendar, Date, Properties}
+import java.util.{Calendar, Date, Properties, UUID}
 
 import com.hzgc.cluster.clustering.KMeansClustering2.LOG
 import com.hzgc.cluster.consumer.PutDataToEs
@@ -13,6 +13,8 @@ import org.apache.log4j.Logger
 import org.apache.spark.mllib.clustering.KMeans
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.sql.SparkSession
+import edu.berkeley.cs.amplab.spark.indexedrdd.IndexedRDD
+import edu.berkeley.cs.amplab.spark.indexedrdd.IndexedRDD._
 
 import scala.collection.mutable
 
@@ -20,11 +22,14 @@ object KMeansClustering {
 
   case class Data(id: Long, time: Timestamp, ipc: String, host: String, spic: String, bpic: String)
 
-  val LOG: Logger = Logger.getLogger(KMeansClustering.getClass)
-  val numClusters = 30
-  val numIterations = 10000
-  var clusterIndex: Int = 0
+  case class CenterData(num: Int, data: Array[Double])
 
+  val LOG: Logger = Logger.getLogger(KMeansClustering.getClass)
+  val numClusters = 50
+  val numIterations = 100000
+  var clusterIndex: Int = 0
+  val similarityThreshold = 0.90
+  val appearCount = 10
 
   def main(args: Array[String]) {
 
@@ -53,12 +58,21 @@ object KMeansClustering {
 
     spark.sql("select ftpurl,feature from person_table where date like " + currentYearMon).createOrReplaceTempView("parquetTable")
 
-    val preSql = "(select T1.id, T2.host_name, T2.big_picture_url, T2.small_picture_url, T1.alarm_time " + "from t_alarm_record as T1 inner join t_alarm_record_extra as T2 on T1.id=T2.record_id " + "where T2.static_id IS NULL " + "and DATE_FORMAT(T1.alarm_time,'%Y-%m') like " + currentYearMon + ") as temp"
+    val preSql = "(select T1.id, T2.host_name, T2.big_picture_url, T2.small_picture_url, T1.alarm_time " +
+      "from t_alarm_record as T1 inner join t_alarm_record_extra as T2 on T1.id=T2.record_id " +
+      "where T2.static_id IS NULL " + "and DATE_FORMAT(T1.alarm_time,'%Y-%m') like " + currentYearMon + ") as temp"
 
     val dataSource = spark.read.jdbc(url, preSql, sqlProper)
     dataSource.map(data => {
-      println("ftp://" + data.getAs[String](hostField) + ":2121/" + data.getAs[String](spicField))
-      Data(data.getAs[Long](idField), data.getAs[Timestamp](timeField), data.getAs[String](spicField).substring(1, data.getAs[String](spicField).indexOf("/", 1)), data.getAs[String](hostField), "ftp://" + data.getAs[String](hostField) + ":2121" + data.getAs[String](spicField), "ftp://" + data.getAs[String](hostField) + ":2121" + data.getAs[String](bpicField))
+      println("ftp://" + data.getAs[String](hostField) + ":2121" + data.getAs[String](spicField))
+      Data(data.getAs[Long](idField),
+        data.getAs[Timestamp](timeField),
+        data.getAs[String](spicField).substring(1, data.getAs[String](spicField).indexOf("/", 1)),
+        data.getAs[String](hostField), "ftp://" +
+          data.getAs[String](hostField) + ":2121" +
+          data.getAs[String](spicField), "ftp://" +
+          data.getAs[String](hostField) + ":2121" +
+          data.getAs[String](bpicField))
     }).createOrReplaceTempView("mysqlTable")
 
     val joinData = spark.sql("select T1.feature, T2.* from parquetTable as T1 inner join mysqlTable as T2 on T1.ftpurl=T2.spic")
@@ -68,91 +82,136 @@ object KMeansClustering {
         Vectors.dense(data.getAs[mutable.WrappedArray[Float]]("feature")
           .toArray.map(_.toDouble))))
       .cache()
-
-    /*val trainData = idPointRDD.map(_._2)
-    val numData = trainData.count()
-    val i = 0
-    val k_wsseMap = new mutable.HashMap[Int, Double]()
-    var max_K = math.sqrt(numData).toInt
-    for (i <- 1 to max_K) {
-      var numClusters = i
-      val kMeansModel = KMeans.train(trainData, numClusters, numIterations)
-      val trainMidResult = kMeansModel.predict(idPointRDD.map(_._2))
-      val wsse = kMeansModel.computeCost(trainData)
-      k_wsseMap.put(numClusters, wsse)
-    }
-    val a = (k_wsseMap(max_K) - k_wsseMap(1)) / (max_K - 1)
-    val b = (max_K * k_wsseMap(1) - k_wsseMap(max_K)) / (max_K - 1)
-    println("k_wsseMap(1):" + k_wsseMap(1))
-    println("k_wsseMap(max_k):" + k_wsseMap(max_K))
-    println("max_K:" + max_K)
-    println("a:" + a)
-    println("b:" + b)
-    spark.sparkContext.broadcast(a)
-    spark.sparkContext.broadcast(b)
-    val dist = new util.ArrayList[Double]()
-    k_wsseMap.foreach(println(_))
-    val distMap = k_wsseMap.map(data =>
-      (data._1, math.abs((a * data._1 - data._2 + b)) / math.sqrt(math.pow(a, 2) + 1))).foreach(println(_))*/
-
+    val centerList = new util.ArrayList[CenterData]()
     val kMeansModel = KMeans.train(idPointRDD.map(data => data._2), numClusters, numIterations)
-      val trainMidResult = kMeansModel.predict(idPointRDD.map(_._2))
-      val viewData = joinData.select("id", "time", "ipc", "host", "spic", "bpic").rdd
-      var trainResult = trainMidResult.zip(viewData)
-        .groupByKey()
-        .sortByKey()
-        .map(data => (data._1, data._2.toArray.sortWith((a, b) => a.getTimestamp(1).getTime > b.getTimestamp(1).getTime)))
-
-      val table1List = new util.ArrayList[ClusteringAttribute]()
-      trainResult.map(data => {
-        val attribute = new ClusteringAttribute()
-        attribute.setClusteringId(data._1.toString)
-        attribute.setCount(data._2.length)
-        attribute.setLastAppearTime(data._2(0).getTimestamp(1).toString)
-        attribute.setLastIpcId(data._2(0).getAs[String]("ipc"))
-        attribute.setFirstAppearTime(data._2(data._2.length - 1).getTimestamp(1).toString)
-        attribute.setFirstIpcId(data._2(data._2.length - 1).getAs[String]("ipc"))
-        attribute.setFtpUrl(data._2(data._2.length / 2).getAs[String]("spic"))
-        attribute
-      }).collect().foreach(data => table1List.add(data))
-
-      val mon = calendar.get(Calendar.MONTH)
-      var monStr = ""
-      if (mon < 10) {
-        monStr = "0" + mon
-      } else {
-        monStr = String.valueOf(mon)
+    kMeansModel.clusterCenters.foreach(x => {
+      println("Center Point of Cluster" + clusterIndex + ":")
+      println(x)
+      centerList.add(new CenterData(clusterIndex, x.toArray))
+      clusterIndex += 1
+    })
+    val centerListTmp = new util.ArrayList[CenterData]()
+    centerListTmp.addAll(centerList)
+    val deleteCenter = new util.ArrayList[Int]()
+    val union_center = new util.HashMap[Int, util.ArrayList[Int]]
+    for (i <- 0 to centerListTmp.size() - 1) {
+      val first = centerListTmp.get(i)
+      if (!deleteCenter.contains(first.num)) {
+        val centerSimilarity = new util.ArrayList[Int]()
+        val iter = centerList.iterator()
+        while (iter.hasNext) {
+          val second = iter.next()
+          val pairSim = cosineMeasure(first.data, second.data)
+          if (pairSim > similarityThreshold) {
+            deleteCenter.add(second.num)
+            centerSimilarity.add(second.num)
+            iter.remove()
+          }
+        }
+        union_center.put(first.num, centerSimilarity)
       }
-      val yearMon = calendar.get(Calendar.YEAR) + "-" + monStr
-      LOG.info("write clustering info to HBase...")
-      PutDataToHBase.putClusteringInfo(yearMon, table1List)
+    }
 
-    /*trainResult.foreach(data => {
-      val rowKey = yearMon + "-" + data._1
+    /* val unionCenterMap = new mutable.HashMap
+     for (i <- 0 to centerSimilarity.size() - 1) {
+       println(centerSimilarity.get(i))
+     }*/
+
+    val dataCenter = idPointRDD.map(_._2).map(p => (kMeansModel.predict(p), kMeansModel.clusterCenters.apply(kMeansModel.predict(p)), p))
+
+    val zipDataCenter = dataCenter.zip(idPointRDD.map(_._2))
+    val point_center_dist = zipDataCenter.map(data => (data._1._1, cosineMeasure(data._1._2.toArray, data._2.toArray)))
+    val viewData = joinData.select("id", "time", "ipc", "host", "spic", "bpic", "feature").rdd
+    val predictResult = point_center_dist.zip(viewData).groupBy(key => key._1._1).mapValues(f => {
+      f.toList.filter(data => data._1._2 > similarityThreshold).sortWith((a, b) => (a._1._2 < b._1._2))
+    })
+      //.filter(data => data._2.length > appearCount)
+    val table1List = new util.ArrayList[ClusteringAttribute]()
+    val uuidString = UUID.randomUUID().toString
+
+    /* val center_Point_map = mutable.HashMap[Int, mutable.WrappedArray[Float]]()
+     val firstImgRDD = predictResult.map(data =>
+       (data._1, data._2(0)._2.getAs[mutable.WrappedArray[Float]]("feature")))
+
+     val zip_firstImgRDD = firstImgRDD.zipWithIndex()
+     val join_zipedfirstImgRDD = zip_firstImgRDD.cartesian(zip_firstImgRDD).filter(f => f._1._2 < f._2._2)
+     join_zipedfirstImgRDD.map(data=>
+       (data._1._1._1,data._2._1._1,cosineMeasure(data._1._1._2.toArray.map(_.toDouble),data._2._1._2.toArray.map(_.toDouble))))
+       .foreach(println(_))
+
+     val indexed = IndexedRDD(predictResult).cache()*/
+    predictResult.sortByKey().collect().foreach(println(_))
+    val indexedResult = IndexedRDD(predictResult).cache()
+
+    /*val iter_center = union_center.keySet().iterator()
+    while (iter_center.hasNext) {
+      val key = iter_center.next()
+      val value = union_center.get(key)
+      val value_iter = value.iterator()
+      while (value_iter.hasNext) {
+        val first_list=indexedResult.get(key).get
+        val cluster_tmp = value_iter.next()
+        val second_list=indexedResult.get(cluster_tmp).get
+        indexedResult.put(key,first_list.union(second_list))
+      }
+    }*/
+
+    /*predictResult.map(data => {
+      val attribute = new ClusteringAttribute()
+      attribute.setClusteringId(data._1.toString + uuidString)
+      attribute.setCount(data._2.length)
+      attribute.setLastAppearTime(data._2(0)._2.getTimestamp(1).toString)
+      attribute.setLastIpcId(data._2(0)._2.getAs[String]("ipc"))
+      attribute.setFirstAppearTime(data._2(data._2.length - 1)._2.getTimestamp(1).toString)
+      attribute.setFirstIpcId(data._2(data._2.length - 1)._2.getAs[String]("ipc"))
+      attribute.setFtpUrl(data._2(0)._2.getAs[String]("spic"))
+      attribute
+    }).collect().foreach(data => table1List.add(data))
+
+    val mon = calendar.get(Calendar.MONTH)
+    var monStr = ""
+    if (mon < 10) {
+      monStr = "0" + mon
+    } else {
+      monStr = String.valueOf(mon)
+    }
+    val yearMon = calendar.get(Calendar.YEAR) + "-" + monStr
+    LOG.info("write clustering info to HBase...")
+    PutDataToHBase.putClusteringInfo(yearMon, table1List)
+
+
+    val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val putDataToEs = PutDataToEs.getInstance()
+    predictResult.foreach(data => {
+      val rowKey = yearMon + "-" + data._1 + uuidString
       println(rowKey)
-      val idList = new util.ArrayList[Integer]()
-      data._2.foreach(data => idList.add(data.getAs[Long]("id").toInt))
-      println(idList)
-      println("++++++++++++++++++++++")
-      PutDataToHBase.putDetailInfo_v1(rowKey, idList)
-    })*/
+      data._2.foreach(p => {
+        val date = new Date(p._2.getAs[Timestamp]("time").getTime)
+        val dateNew = sdf.format(date)
+        val status = putDataToEs.upDateDataToEs(p._2.getAs[String]("spic"), rowKey, dateNew, p._2.getAs[Long]("id").toInt)
+        if (status != 200) {
+          LOG.info("Put data to es failed! And the failed ftpurl is " + p._2.getAs("spic"))
+        }
+      })
+    }) */
+      spark.stop()
+  }
 
+  def cosineMeasure(v1: Array[Double], v2: Array[Double]): Double = {
 
-     val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-     val putDataToEs = PutDataToEs.getInstance()
-     trainResult.foreach(data => {
-       val rowKey = yearMon + "-" + data._1
-       println(rowKey)
-       data._2.foreach(data => {
-         val date = new Date(data.getAs[Timestamp]("time").getTime)
-         val dateNew = sdf.format(date)
-         val status = putDataToEs.upDateDataToEs(data.getAs[String]("spic"), rowKey, dateNew, data.getAs[Long]("id").toInt)
-         if (status != 200) {
-           LOG.info("Put data to es failed! And the failed ftpurl is " + data.getAs("spic"))
-         }
-       })
-     })
-    spark.stop()
+    val member = v1.zip(v2).map(d => d._1 * d._2).reduce(_ + _).toDouble
+    //求出分母第一个变量值
+    val temp1 = math.sqrt(v1.map(num => {
+      math.pow(num, 2)
+    }).reduce(_ + _))
+    //求出分母第二个变量值
+    val temp2 = math.sqrt(v2.map(num => {
+      math.pow(num, 2)
+    }).reduce(_ + _))
+    //求出分母
+    val denominator = temp1 * temp2
+    //进行计算
+    0.5 + 0.5 * (member / denominator)
   }
 }
 
